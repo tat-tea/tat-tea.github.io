b5d7c99538957eaa9daeb5b496baea365409d664
tat-tea <dream2catch@gmail.com>
2017-10-22 04:21:19 +0900

    update

--- mapscript.js
+++ mapscript.js
@@ -1,36 +1,27 @@
 	var map;
-	var data;
-	
-	//load json
-	alert('いくぜ');
-	var req = new XMLHttpRequest();
-	req.open('GET', 'station.json');
-	req.onreadystatechange = function(event){
-		if(req.readyState == 4){
-			var div = document.getElementById('test');
-			if(req.status == 200){
-				data = eval( '(' + req.responseText + ')' );
-			} else {
-				alert('json読み込みエラー');
-			}
-		}
-	
-	};
-	req.send();
-	
 	function initMap() {
 		map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: 35.681391, lng: 139.766103} ,
 		zoom: 10
 		});
 		
-		for(var i = 0; i < data.length; i++){
-					
-			var marker = new google.maps.Marker({
-			    position: (data[i].lat, data[i].lng),
-			    map: map,
-			    name: data[i].name
-		  	});
-		}
-	
+		var data;
+		var req = new XMLHttpRequest();
+		req.open('GET', 'station.json');
+		req.onreadystatechange = function(event){
+			if(req.readyState == 4){
+				if(req.status == 200){
+					data = eval( '(' + req.responseText + ')' );
+					for(var i = 0; i < data.length; i++){
+						var marker = new google.maps.Marker({
+						    position: (data[i].lat, data[i].lng),
+						    name: data[i].name
+						    map: map
+					  	});
+					}
+				} else {
+					alert('json読み込みエラー');
+				}
+			}
+		};
 	}
