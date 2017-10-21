bcc75d8d75bb098e776d5b5814e09a890b1e6fb7
tat-tea <dream2catch@gmail.com>
2017-10-22 01:40:50 +0900

    update

--- mapscript.js
+++ mapscript.js
@@ -16,19 +16,34 @@ var marker = [];
 	}
 	];
 
+function downloadData(){
+  GDownloadUrl("station.json", function dispData(data, statusCode){
+    var obj = eval("(" + data + ")");
+
+    for (var i = 0; i < obj.marker.length; i++) {
+      var lat = obj.marker[i].lat;
+      var lng = obj.marker[i].lng;
+      var name = obj.marker[i].name;
+
+      map.addOverlay(createMarker(lat, lng, name));
+    }
+  });
+}
+
+function createMarker(lat, lng, name) {
+  var marker = new GMarker(new GLatLng(lat, lng));
+
+  var html = "<p>" + name + "</p>";
+  GEvent.addListener(marker, "click", function(){
+    marker.openInfoWindowHtml(html);
+  });
+
+  return marker;
+}
+
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
 	center: {lat: 35.681391, lng: 139.766103} ,
 	zoom: 10
 });
-
-for (var i = 0; i < data.length; i++) {
-	markerLatLng = {lat: data[i]['lat'], lng: data[i]['lng']};
-	strtitle = {name: markerLatLng}
-	marker[i] = new google.maps.Marker({
-	position: markerLatLng,
-	map: map,
-	title: data[i]['name']
-});
-}
 }
