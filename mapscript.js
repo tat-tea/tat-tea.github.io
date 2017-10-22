2bf471eb305e07310baf71ec3f45e5d554370b35
tat-tea <dream2catch@gmail.com>
2017-10-22 22:09:57 +0900

    aa
    aa

--- mapscript.js
+++ mapscript.js
@@ -21,7 +21,6 @@ function initMap() {
 		    	markerLatLng = new google.maps.LatLng({lat: markerData[i].lat, lng: markerData[i].lng}); // 緯度経度のデータ作成
 		    	marker[i] = new google.maps.Marker({ // マーカーの追加
 		    	position: markerLatLng, // マーカーを立てる位置を指定
-		    	map: map // マーカーを立てる地図を指定
 				  	});
 				}
 			} else {
