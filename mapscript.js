f38d70e560d1fb7a37e064efda0b1f2afbf8d32e
tat-tea <dream2catch@gmail.com>
2017-10-23 00:58:53 +0900

    a
    a

--- mapscript.js
+++ mapscript.js
@@ -1,6 +1,35 @@
 var map;
 var marker = [];
 var data = [];
+var markerData = [ // マーカーを立てる場所名・緯度・経度
+	{
+		name : 'TAM 東京',
+		lat : 35.6954806,
+		lng : 139.76325010000005,
+		icon : 'tam.png' // TAM 東京のマーカーだけイメージを変更する
+	}, {
+		name : '小川町駅',
+		lat : 35.6951212,
+		lng : 139.76610649999998
+	}, {
+		name : '淡路町駅',
+		lat : 35.69496,
+		lng : 139.76746000000003
+	}, {
+		name : '御茶ノ水駅',
+		lat : 35.6993529,
+		lng : 139.76526949999993
+	}, {
+		name : '神保町駅',
+		lat : 35.695932,
+		lng : 139.75762699999996
+	}, {
+		name : '新御茶ノ水駅',
+		lat : 35.696932,
+		lng : 139.76543200000003
+	}
+];
+
 function initMap() {
 	var myLatlng = new google.maps.LatLng(35.170981, 136.881556);
 	var mapOptions = {
@@ -29,17 +58,16 @@ function initMap() {
 		async : true
 	});
 
-	var next = new google.maps.LatLng(140.7, 41.01);
-	var next2 = new google.maps.LatLng(data[0].lat, data[0].lng);
-
-	var marker = new google.maps.Marker({
-		position : next,
-		title : data[0].name
-	});
-
-	// To add the marker to the map, call setMap();
-	marker.setMap(map);
-
+	for (var i = 0; i < markerData.length; i++) {
+		markerLatLng = new google.maps.LatLng({
+			lat : markerData[i]['lat'],
+			lng : markerData[i]['lng']
+		}); // 緯度経度のデータ作成
+		marker[i] = new google.maps.Marker({ // マーカーの追加
+			position : markerLatLng, // マーカーを立てる位置を指定
+			map : map // マーカーを立てる地図を指定
+		});
+	}
 	/*
 
 	map = new google.maps.Map(document.getElementById('map'), {
