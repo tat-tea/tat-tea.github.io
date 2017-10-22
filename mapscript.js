953ab3d6796d32689677ea7babe36f2cc869ed05
tat-tea <dream2catch@gmail.com>
2017-10-22 22:28:36 +0900

    a
    a

--- mapscript.js
+++ mapscript.js
@@ -1,52 +1,27 @@
 var map;
 var marker = [];
-
-
+var data = [];
 function initMap() {
-	map = new google.maps.Map(document.getElementById('map'), {
-	center: {lat: 35.681391, lng: 139.766103},
-	zoom: 10
-	});
-	
-	// マーカー毎の処理
-	
-	var markerdata;
-	var req = new XMLHttpRequest();
-	req.open('GET', 'station.json');
-	req.onreadystatechange = function(event){
-		if(req.readyState == 4){
-			if(req.status == 200){
-				data = eval( '(' + req.responseText + ')' );
-				for(var i = 0; i < data.length; i++){
-		    	markerLatLng = new google.maps.LatLng({lat: markerData[i].lat, lng: markerData[i].lng}); // 緯度経度のデータ作成
-		    	marker[i] = new google.maps.Marker({ // マーカーの追加
-		    	position: markerLatLng, // マーカーを立てる位置を指定
-				  	});
-				}
-			} else {
-				alert('json読み込みエラー');
-			}
-		}
-	}
+map = new google.maps.Map(document.getElementById('map'), {
+center: {lat: 35.170981, lng: 136.881556} ,
+zoom: 15
+});
+$.getJSON("station.json", function(json){
+for (var i = 0; i <= json.length-1; i++) {
+data.push(
+{
+'name': json[i].name,
+'lat': json[i].lat,
+'lng': json[i].lng
 }
-
-function getJSON() {
-	// XMLHttpRequest オブジェクトを生成する
-	var req = new XMLHttpRequest();
-	
-	// XMLHttpRequest オブジェクトの状態が変化した際に呼び出されるイベントハンドラ
-	req.onreadystatechange = function() {
-	
-		// サーバーからのレスポンスが完了し、かつ、通信が正常に終了した場合
-		if(req.readyState == 4 && req.status == 200){
-			// 取得した JSON ファイルの中身を表示
-			var elem = eval('(' + req.responseText + ')');
-			alert(elem.length);
-		}
-	};
-	// HTTPメソッドとアクセスするサーバーの　URL　を指定
-	req.open("GET", "station.json"); 
-	// 実際にサーバーへリクエストを送信
-	req.send(null); 
-	
+);
+};
+for (var i = 0; i < data.length; i++) {
+markerLatLng = {lat: data[i]['lat'], lng: data[i]['lng']};
+marker[i] = new google.maps.Marker({
+position: markerLatLng,
+map: map
+});
+}
+});//jsonの閉じ
 }
