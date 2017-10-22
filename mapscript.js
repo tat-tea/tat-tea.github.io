6c982e13e03d137907c9524486a1c4806b9ba2eb
tat-tea <dream2catch@gmail.com>
2017-10-22 22:08:37 +0900

    tat-tea
    sample commit

--- mapscript.js
+++ mapscript.js
@@ -1,27 +1,43 @@
-	var map;
-	function initMap() {
-		map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 35.681391, lng: 139.766103} ,
-		zoom: 10
-		});
-		
-		var data;
-		var req = new XMLHttpRequest();
-		req.open('GET', 'station.json');
-		req.onreadystatechange = function(event){
-			if(req.readyState == 4){
-				if(req.status == 200){
-					data = eval( '(' + req.responseText + ')' );
-					for(var i = 0; i < data.length; i++){
-						var marker = new google.maps.Marker({
-						    position: (data[i].lat, data[i].lng),
-						    name: data[i].name
-						    map: map
-					  	});
-					}
-				} else {
-					alert('json読み込みエラー');
+var map;
+var marker = [];
+
+
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+	center: {lat: 35.681391, lng: 139.766103},
+	zoom: 10
+	});
+	
+	// マーカー毎の処理
+	
+	var markerdata;
+	var req = new XMLHttpRequest();
+	req.open('GET', 'station.json');
+	req.onreadystatechange = function(event){
+		if(req.readyState == 4){
+			if(req.status == 200){
+				data = eval( '(' + req.responseText + ')' );
+				for(var i = 0; i < data.length; i++){
+		    	markerLatLng = new google.maps.LatLng({lat: markerData[i].lat, lng: markerData[i].lng}); // 緯度経度のデータ作成
+		    	marker[i] = new google.maps.Marker({ // マーカーの追加
+		    	position: markerLatLng, // マーカーを立てる位置を指定
+		    	map: map // マーカーを立てる地図を指定
+				  	});
 				}
+			} else {
+				alert('json読み込みエラー');
 			}
-		};
+		}
 	}
+}
+
+function getJSON() {
+  var req = new XMLHttpRequest();		  // XMLHttpRequest オブジェクトを生成する
+  req.onreadystatechange = function() {		  // XMLHttpRequest オブジェクトの状態が変化した際に呼び出されるイベントハンドラ
+    if(req.readyState == 4 && req.status == 200){ // サーバーからのレスポンスが完了し、かつ、通信が正常に終了した場合
+      alert(req.responseText);		          // 取得した JSON ファイルの中身を表示
+    }
+  };
+  req.open("GET", "station.json", false); // HTTPメソッドとアクセスするサーバーの　URL　を指定
+  req.send(null);					    // 実際にサーバーへリクエストを送信
+}
