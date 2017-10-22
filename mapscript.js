bbcb74afd28072a2d10a6745dc5b053b51f75568
tat-tea <dream2catch@gmail.com>
2017-10-23 00:45:34 +0900

    a
    a

--- mapscript.js
+++ mapscript.js
@@ -2,6 +2,23 @@ var map;
 var marker = [];
 var data = [];
 function initMap() {
+	var myLatlng = new google.maps.LatLng(-25.363882, 131.044922);
+	var mapOptions = {
+		zoom : 4,
+		center : myLatlng
+	}
+	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+	var marker = new google.maps.Marker({
+		position : myLatlng,
+		title : "Hello World!"
+	});
+
+	// To add the marker to the map, call setMap();
+	marker.setMap(map);
+
+	/*
+
 	map = new google.maps.Map(document.getElementById('map'), {
 		center : {
 			lat : 35.170981,
@@ -46,5 +63,7 @@ function initMap() {
 		marker.setMap(map);
 
 	}
+
+	*/
 
 }
