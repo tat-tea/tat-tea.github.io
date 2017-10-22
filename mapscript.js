f3e3ff100e687b8fb1050fa2a9186d727ee33345
tat-tea <dream2catch@gmail.com>
2017-10-23 00:26:23 +0900

    s
    s

--- mapscript.js
+++ mapscript.js
@@ -20,37 +20,23 @@ function initMap() {
 			);
 		}
 		;
-		for (var i = 0; i < data.length; i++) {
-			var lat = data[i].lat;
-			var lng = data[i].lng;
-			var latlng = new google.maps.LatLng(140, 41);
-			var marker = createMarker(data[i].name, latlng);
-
-			markerLatLng = {
-				lat : data[i].lat,
-				lng : data[i].lng
-			};
-			marker[i] = new google.maps.Marker({
-				position : markerLatLng,
-				map : map
-			});
-		}
+//		for (var i = 0; i < data.length; i++) {
+//			var lat = data[i].lat;
+//			var lng = data[i].lng;
+//			var latlng = new google.maps.LatLng(140, 41);
+//			var marker = createMarker(data[i].name, latlng);
+//
+//			markerLatLng = {
+//				lat : data[i].lat,
+//				lng : data[i].lng
+//			};
+//			marker[i] = new google.maps.Marker({
+//				position : markerLatLng,
+//				map : map
+//			});
+//		}
 	}); //jsonの閉じ
 
 
-	function createMarker(name, latlng) {
-		var marker = new google.maps.Marker({
-			position : latlng,
-			animation : google.maps.Animation.DROP,
-			map : map
-		});
-		google.maps.event.addListener(marker, "click", function() {
-			if (infowindow) infowindow.close();
-			infowindow = new google.maps.InfoWindow({
-				content : name
-			});
-			infowindow.open(map, marker);
-		});
-		return marker;
-	}
+	alert(data.length);
 }
