d0be9dade4a41cd7b179e82dfb296b2d1332473b
tat-tea <dream2catch@gmail.com>
2017-10-23 00:13:50 +0900

    s
    s

--- mapscript.js
+++ mapscript.js
@@ -3,25 +3,35 @@ var marker = [];
 var data = [];
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
-	center: {lat: 35.170981, lng: 136.881556} ,
-	zoom: 15
+		center : {
+			lat : 35.170981,
+			lng : 136.881556
+		},
+		zoom : 15
 	});
-	$.getJSON("station.json", function(json){
-	for (var i = 0; i <= json.length-1; i++) {
-		data.push(
-			{
-			'name': json[i].name,
-			'lat': json[i].lat,
-			'lng': json[i].lng
-			}
-		);
-	};
+
+	$.getJSON("station.json", function(json) {
+		for (var i = 0; i <= json.length - 1; i++) {
+			data.push(
+				{
+					'name' : json[i].name,
+					'lat' : json[i].lat,
+					'lng' : json[i].lng
+				}
+			);
+		}
+		;
+	}); //jsonの閉じ
+
 	for (var i = 0; i < data.length; i++) {
-		markerLatLng = {lat: data[i]['lat'], lng: data[i]['lng']};
-		strtitle = {name: markerLatLng}
+		markerLatLng = {
+			lat : data[i]['lat'],
+			lng : data[i]['lng']
+		};
 		marker[i] = new google.maps.Marker({
-		position: markerLatLng,
-		map: map,
-		title: data[i]['name']
-	});//jsonの閉じ
+			position : markerLatLng,
+			map : map,
+			title : data[i]['name']
+		});
+	}
 }
